2aa72a10f2214ce6ca9ca48ec1373cc15efcbf03
hantarr01 <thunderbolt655@mail.ru>
2018-10-16 09:44:45 +0300

    Team work

---
+++ js/script.js
@@ -0,0 +1,347 @@
+/*
+$(document).ready(function(){
+$("tlogo").add("h1").css("background-color","green")
+    $("#f1").click(function(){
+      $("#par1").remove();
+        $("#f2").click(function(){
+      $("h1").remove();
+   });
+   $("#f3").click(function(){
+      $("p").remove();
+   });
+   });
+    });
+$(document).ready(function(){
+$(":button").click(function(){
+     $(":button").hide();
+  });
+   });
+
+$(document).ready(function(){
+$("#p1").mouseover(function(){
+     $(this).css("color","lime");
+      });
+     $("#p1").mouseout(function(){
+     $(this).css("color","black");
+  });
+  });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*var s = function (str){
+    var bar = str.slice(4)
+    return bar;
+                      }
+console.log (s("Амортенция")); 
+    var g = function (str){
+	var gor = str.charAt(0)
+	return gor;
+}
+console.log (g("Vodomir"));
+
+var m = function (str){
+	var myth = str.charAt(s.length-1);
+	return myth;
+                      }
+console.log (m("Дейли Бьюгл"));
+
+var o = function (str){
+	var tort = str.substring (1,5)
+	return tort;
+                      }
+//console.log (o("Индастриз"));
+
+var e = function (str){
+	var bus = str.slice (-3)
+	return bus;
+                      }
+console.log (e("Алхимия"));
+
+var f = function (str){
+	var pab = str.indexOf("l")
+	return pab;
+                      }
+console.log (f("Скрытность"));
+
+var q = function(str){
+	var gaz = str.lastIndexOf("l")
+	return gaz;
+                     }
+console.log (q("Пипидастр"));
+
+var h = function(str){
+	var topaz = str.indexOf ("l", 3)
+	return topaz;
+                     }
+console.log (h("Лимонные дольки"));
+
+var v = function (str){
+	var almaz = str.split (", ")
+	return almaz;
+                      }
+console.log (v(""));
+
+var l = function (str){
+	var emerald = str.replace ("h", "H")
+	return emerald;
+                      }
+console.log (l("Флеш-бэнг"));    
+
+var p = function (str){
+	var thunder = str.toUpperCase()
+	return thunder;
+                      }                  
+console.log (p("Веном"))
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ 
+
+
+
+
+
+
+
+
+
